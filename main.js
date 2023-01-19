7c1b7762d938623918dc98c2f67f0562cbc70c40
Nguerre11 <nguerrerovgit@gmail.com>
2023-01-19 15:47:10 -0500

    start-arr-main

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 const menu = document.querySelector('.desktop-menu');
 const navEmail = document.querySelector('.nav-email');
-const menuMobile = document.querySelector('.mobile-menu')
-const IconHam = document.querySelector('.menu')
+const menuMobile = document.querySelector('.mobile-menu');
+const IconHam = document.querySelector('.menu');
+/* checkout-shopping-cart */
+const cart = document.querySelector('.shopping-car');
+const aside = document.querySelector('.section-container');
+/* defined inactive-stated */
+
+
+
 navEmail.addEventListener('click',block);
-IconHam.addEventListener('click',blocks);
+IconHam.addEventListener('click',blockMobile);
+cart.addEventListener('click',blockCart);
+
+
 function block()
 {
     menu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
 
-function blocks()
-{
+
+function blockMobile()
+{ 
     menuMobile.classList.toggle('inactive');
+     aside.classList.add('inactive');
+}
+
+function blockCart()
+{
+    aside.classList.toggle('inactive');
+    menuMobile.classList.add('inactive');
+    menu.classList.add('inactive');
 
 }
 
